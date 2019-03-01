b6ca97b4778998468097648082a118636113c87a
Ken Tan <ken11zer01@gmail.com>
2019-03-01 12:39:17 +0800

    Create router

---
+++ src/router/routes.js
@@ -0,0 +1,12 @@
+const dashboard = () => import('../pages/Dashboard');
+
+export default {
+  dashboad: {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: dashboard,
+    meta: {
+
+    }
+  }
+}
