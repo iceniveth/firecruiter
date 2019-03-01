e84784a2c83cb23fcd09a104a6322dcc9e1c1387
Ken Tan <ken11zer01@gmail.com>
2019-03-01 13:41:08 +0800

    Add navigation

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,16 @@
     <v-content>
       <router-view :key="$route.fullPath" />
     </v-content>
+    <the-navigation />
   </v-app>
 </template>
 
 <script>
+import TheNavigation from './components/TheNavigation';
+
 export default {
-  
+  components: {
+    TheNavigation
+  }
 };
 </script>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-navigation-drawer permanent app>
+    <v-toolbar dense flat>
+      <!-- <v-avatar class="px-0 mx-0">
+        <v-icon>add</v-icon>
+      </v-avatar> -->
+      <v-toolbar-title>Firecruiter</v-toolbar-title>
+    </v-toolbar>
+
+    <v-divider></v-divider>
+
+    <v-list class="py-0">
+      <v-list-tile v-for="link in links"
+                   :key="link.text"
+                   @click="$router.push({ name: link.name })">
+        <v-list-tile-avatar>
+          <v-avatar>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-avatar>
+        </v-list-tile-avatar>
+        
+        <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import routes from '../router/routes';
+
+export default {
+  computed: {
+    links() {
+      return Object.keys(routes)
+        .filter(key => routes[key].meta.navigation != null)
+        .map(key => Object.assign({}, {
+          ...routes[key].meta.navigation,
+          name: routes[key].name
+        }));
+    }
+  }
+};
+</script>
+
