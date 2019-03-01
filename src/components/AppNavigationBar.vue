f731958fb738ae575a2c93af8ceb740d27cc37d2
Ken Tan <ken11zer01@gmail.com>
2019-03-01 14:21:25 +0800

    Add navigation bar

---
+++ src/components/AppNavigationBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-toolbar dense>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="ml-2">{{ title }}</v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import router from './router';
 import App from './App.vue';
+import AppNavigationBar from './components/AppNavigationBar';
 
 Vue.use(Vuetify);
+
+Vue.component('app-navigation-bar', AppNavigationBar);
 
 new Vue({
   el: '#app',
