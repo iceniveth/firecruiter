b7159f3f0fa1009b433f4050590a70acb0ba9e28
Ken Tan <ken11zer01@gmail.com>
2019-03-05 18:20:03 +0800

    Add theme primary and accent colors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
 import router from './router';
 import store from './store';
 import App from './App.vue';
 import AppNavigationBar from './components/AppNavigationBar';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.blue.darken4,
+    primaryText: colors.shades.white,
+    accent: colors.orange.accent3,
+    accentText: colors.shades.black
+  }
+});
 
 Vue.component('app-navigation-bar', AppNavigationBar);
 
