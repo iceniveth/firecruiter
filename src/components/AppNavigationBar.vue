210e81483a7cabe2f8de9769d1acc3a85e64049c
Ken Tan <ken11zer01@gmail.com>
2019-03-05 13:43:33 +0800

    Show search bar on search mode

--- src/components/AppNavigationBar.vue
+++ src/components/AppNavigationBar.vue
@@ -1,35 +1,69 @@
 <template>
-  <v-toolbar dense>
-    <v-toolbar-side-icon @click="$store.commit('setDrawer', true)"></v-toolbar-side-icon>
-    <v-toolbar-title class="ml-2">{{ title }}</v-toolbar-title>
-    <v-spacer></v-spacer>
+  <v-toolbar app
+             dense
+             class="mx-0 px-0">
+    <v-toolbar-side-icon v-if="!searchMode"
+                         @click="$store.commit('setDrawer', true)"></v-toolbar-side-icon>
+    <v-toolbar-title v-if="!searchMode"
+                     class="ml-2">{{ title }}</v-toolbar-title>
 
-    <v-tooltip bottom>
+
+    <v-tooltip bottom
+               v-if="searchMode">
       <v-btn icon
-             slot="activator">
+             slot="activator"
+             v-if="searchMode"
+             @click="searchMode = false">
+        <v-icon>keyboard_arrow_left</v-icon>
+      </v-btn>
+      <span>Cancel Search</span>
+    </v-tooltip>
+
+    <v-text-field prepend-inner-icon="search"
+                  solo
+                  flat
+                  clearable
+                  placeholder="Search"
+                  class="pt-2"
+                  v-if="searchMode"></v-text-field>
+    
+    <v-spacer v-if="!searchMode"></v-spacer>
+
+    <v-tooltip bottom
+               v-if="!searchMode">
+      <v-btn icon
+             v-if="!searchMode"
+             slot="activator"
+             @click="searchMode = true">
         <v-icon>search</v-icon>
       </v-btn>
       <span>Search</span>
     </v-tooltip>
     
-    <v-tooltip bottom>
+    <v-tooltip bottom
+               v-if="!searchMode">
       <v-btn icon
+             v-if="!searchMode"
              slot="activator">
         <v-icon>filter_list</v-icon>
       </v-btn>
       <span>Filter</span>
     </v-tooltip>
     
-    <v-tooltip bottom>
+    <v-tooltip bottom
+               v-if="!searchMode">
       <v-btn icon
+             v-if="!searchMode"
              slot="activator">
         <v-icon>delete</v-icon>
       </v-btn>
       <span>Delete</span>
     </v-tooltip>
 
-    <v-tooltip bottom>
+    <v-tooltip bottom
+               v-if="!searchMode">
       <v-btn icon
+             v-if="!searchMode"
              slot="activator">
         <v-icon>more_vert</v-icon>
       </v-btn>
@@ -44,6 +78,11 @@ export default {
     title: {
       type: String,
       required: true
+    }
+  },
+  data() {
+    return {
+      searchMode: false
     }
   }
 }
